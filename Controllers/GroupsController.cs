5cc5c63af8bed4a3cdcdcc2a5807ea20a7bf3051
Edwin <eperez@prokarma.com>
2020-07-04 11:51:15 -0600

    Separate sections into controllers

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassRoomManager.Models;
+using ClassRoomManager.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRoomManager.Controllers
+{
+    [Route("groups")]
+    public class GroupsController : Controller
+    {
+        public IClassRoomManagerData ClassRoomManagerData { get; }
+        public GroupsController(IClassRoomManagerData classRoomManagerData)
+        {
+            ClassRoomManagerData = classRoomManagerData;
+        }
+
+        [Route("list/{groupId:int}", Name = "GroupList")]
+        public IActionResult List(int groupId)
+        {
+            return View(new ListViewModel
+            {
+                Students = ClassRoomManagerData.GetStudentsByGroupId(groupId),
+                Teams = ClassRoomManagerData.GetTeamsByGroupId(groupId),
+                GroupId = groupId
+            });
+        }
+        [Route("list/{groupId:int}", Name = "GroupListPost")]
+        [HttpPost]
+        public IActionResult List(Team team, int groupId)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ClassRoomManagerData.AddTeam(team);
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError("error", ex.Message);
+                }
+
+            }
+            return RedirectToAction("List", new { groupid = groupId });
+        }
+    }
+}
---
+++ Controllers/NotesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassRoomManager.Models;
+using ClassRoomManager.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRoomManager.Controllers
+{
+    [Route("notes")]
+    public class NotesController : Controller
+    {
+        public IClassRoomManagerData ClassRoomManagerData { get; }
+        public NotesController(IClassRoomManagerData classRoomManagerData)
+        {
+            ClassRoomManagerData = classRoomManagerData;
+        }
+
+        [Route("edit/{noteId:int}", Name = "EditNoteForm")]
+        public IActionResult EditNote(int noteId)
+        {
+            return View(ClassRoomManagerData.GetNoteById(noteId));
+        }
+
+        [Route("edit/{noteId:int}", Name = "EditNote")]
+        [HttpPost]
+        public IActionResult EditNote(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ClassRoomManagerData.UpdateNote(note);
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+
+            return View(note);
+        }
+
+        [Route("{groupId?}", Name = "Notes")]
+        public IActionResult Notes(int groupId)
+        {
+            IEnumerable<Note> notes = groupId == 0
+                ? ClassRoomManagerData.GetAllNotes()
+                : ClassRoomManagerData.GetNotesByGroupId(groupId);
+
+            var notesViewModel = new NotesViewModel()
+            {
+                Notes = notes,
+                GroupId = groupId
+            };
+
+            return View(notesViewModel);
+        }
+        [Route("create/{groupId:int}", Name = "CreateNoteForm")]
+        public IActionResult Create(int groupId)
+        {
+            var t = new CreateNoteViewModel(
+                ClassRoomManagerData.GetStudentsByGroupId(groupId),
+                groupId
+            );
+            return View(t);
+        }
+        [HttpPost]
+        [Route("create/{groupId:int}", Name = "CreateNote")]
+        public IActionResult Create(Note note, int groupId)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ClassRoomManagerData.AddNote(note);
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError("error", ex.Message);
+                }
+
+            }
+            return RedirectToAction("Notes", new { groupid = groupId });
+        }
+    }
+}
