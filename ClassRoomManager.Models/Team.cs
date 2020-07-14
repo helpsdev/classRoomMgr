8a07f39a866a9c9a750645be3ec38d45c72336a6
Edwin <eperez@prokarma.com>
2020-07-13 21:06:25 -0600

    add a list of student ids to map the team to a group of students

--- ClassRoomManager.Models/Team.cs
+++ ClassRoomManager.Models/Team.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ClassRoomManager.Models
@@ -10,6 +11,8 @@ namespace ClassRoomManager.Models
         [Display(Name="Nombre")]
         public string Name { get; set; }
         public string Color { get; set; }
+        [NotMapped]
+        public IEnumerable<int> StudentIds { get; set; }
         public IEnumerable<Student> StudentList { get; set; }
         public int TeamId { get; set; }
     }
